56abfb59604e05351a5a672755a0a87e3a8689f5
Bill DeRose <bill.derose11+bderose@gmail.com>
2017-10-21 18:20:09 -0700

    update logging formatter

--- src/service/dexnet_service.py
+++ src/service/dexnet_service.py
@@ -162,7 +162,8 @@ def setup_logging():
     log_file = "{0}/dnaas.log".format(consts.CACHE_DIR)
     handler = logging.FileHandler(log_file)
     handler.setLevel(logging.INFO)
-    formatter = logging.Formatter("%(asctime)s - %(name)s - %(levelname)s - %(message)s")
+    formatter = formatter = logging.Formatter(
+        "[%(asctime)s] {%(pathname)s:%(lineno)d} %(levelname)s - %(message)s")
     handler.setFormatter(formatter)
     app.logger.addHandler(handler)
 
