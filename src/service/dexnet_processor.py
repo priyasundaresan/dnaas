ce6973d26187015467c94c27cbc51772451181ca
BinaryHalibut <alanpusongli@gmail.com>
2018-02-19 08:02:46 -0800

    don't error if config not provided

--- src/service/dexnet_processor.py
+++ src/service/dexnet_processor.py
@@ -156,12 +156,13 @@ def preprocess_mesh(mesh_id, params, progress_reporter_big=lambda x: None, progr
     progress_reporter_big('preprocessing')
 
     gripper_params = params['gripper']
-    config_updates = params['config']
 
     config = {}
     consts._deep_update_config(config, consts.CONFIG)
-    consts._deep_update_config(config, config_updates)
-    config['cache_dir'] = consts.CONFIG['cache_dir']
+    if 'config' in params.keys():
+        config_updates = params['config']
+        consts._deep_update_config(config, config_updates)
+        config['cache_dir'] = consts.CONFIG['cache_dir']
 
 
     # Update gripper params with defaults
