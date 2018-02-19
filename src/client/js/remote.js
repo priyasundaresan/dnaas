2f2355292a5cc17bbd073b3642360fb6386ab12c
BinaryHalibut <alanpusongli@gmail.com>
2018-02-19 08:10:52 -0800

    Add template/locations for metric, config passing in remote.js

--- src/client/js/remote.js
+++ src/client/js/remote.js
@@ -28,7 +28,7 @@ REMOTE.uploadMesh = function() {
     }
     var formData = new FormData();
     formData.set("file", MESH.mesh_main_file);
-    formData.set("params", JSON.stringify({"gripper" : GRIPPER.params}));
+    formData.set("params", JSON.stringify({"metric" : "robust_ferrari_canny", "config" : {}, "gripper" : GRIPPER.params}));
     xhr.open("POST", "http://automation.berkeley.edu/dex-net-api/upload-mesh");
     xhr.send(formData);
 }
